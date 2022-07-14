edd6e7af8b48e826d808e77d4ec4cf12b757e2ff
Anton Barinov <anton@barinov.com>
2022-07-14 22:20:55 +0400

    refactoring

--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -7,13 +7,27 @@ import { useLayoutEffect } from 'react';
  */
 export function useLockBodyScroll() {
     useLayoutEffect(() => {
+        const scrollTop = window.pageYOffset || window.scrollY;
         // Get original body overflow
         const originalStyle = window.getComputedStyle(document.body).overflow;
         // Prevent scrolling on mount
         document.body.style.overflow = 'hidden';
+        document.body.style.position = 'fixed';
+        document.body.style.left = '0';
+        document.body.style.right = '0';
+        document.body.style.bottom = '0';
+        document.body.style.top = (scrollTop * -1) + 'px';
         // Re-enable scrolling when component unmounts
+
         return () => {
             document.body.style.overflow = originalStyle;
+            document.body.style.position = '';
+            document.body.style.left = '';
+            document.body.style.right = '';
+            document.body.style.top = '';
+            document.body.style.bottom = '';
+
+            window.scrollTo(0, scrollTop);
         };
     }, []);
 }
